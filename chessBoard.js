6fd49bfe66a2eebafb48b778575d9dbce1783a08
whediger <wes.hediger@gmail.com>
2016-01-04 00:25:00 -0700

    final chessboard

--- chessBoard.js
+++ chessBoard.js
@@ -3,29 +3,36 @@
 function chessBoard(size) {
 
 	var row = " ";
-	console.log("row length: " + row.length);
+	
+	
+	function spaceShifter() {
+		
+		//shift adding a space or # in row
+			if (row.charAt(0) === " " ){
+				row = "#" + row;
+			} else {
+				row = " " + row;
+			}
+			
+			row = row.substring(0, size);
+	};			
+	
 	
 	//fill initial row with spaces and # or shift charecters
-	for ( i = 0; row.length < size; i++) { spaceShifter(); };
+	for ( ii = 0; row.length < size ; ii++) {
 	
-	//shift adding a space or # in row
-	function spaceShifter() {
-		if (row.charAt(0) === " " ){
-			row = "#" + row;
-		} else {
-			row = " " + row;
-		}
+		spaceShifter();	
 		
-		row = row.substring(0,size);
 	};
-	
-	//create rows and send them to terminal output
+		
+	//shift rows and send them to terminal output
 	for ( i = 0 ; i < size; i++) {
 		
 		spaceShifter();
 		
 		console.log(row);
 	}
+	
 };
 
 //call chessBoard function for testing purposes
