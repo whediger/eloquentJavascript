57c13c9b045a7b5b80b6bb356fb50764c9b6f6a7
whediger <wes.hediger@gmail.com>
2016-01-06 20:34:52 -0700

    recursion final version

--- recursion.js
+++ recursion.js
@@ -1,4 +1,23 @@
+
+
+var result;
+
 function isEven(number){
 	
-	
-}
+	if (number < 0 ) {
+		number *=	-1;
+	} else if ( number == 0 ){
+		result = true;
+	} else if ( number == 1 ){
+		result = false;
+	} else {
+		number -= 2;
+		isEven(number);
+	};
+	return result;
+};
+
+//console logs for testing
+console.log("is even(50): " + isEven(50));
+console.log("is even(75): " + isEven(75));
+console.log("is even(-777)" + isEven(-777));
