0ecf6e0deef2c98f7556c4ca713472fbeefb1cff
whediger <wes.hediger@gmail.com>
2016-01-07 21:31:33 -0700

    beanCounting final

--- beanCounting.js
+++ beanCounting.js
@@ -2,11 +2,21 @@
 
 function countBs(stringIn){
 	
+	return countChar(stringIn, "B");
+};
+
+function countChar(stringIn, charIn){
+	
+	console.log("charIn: " + charIn);
+	
 	var count = 0;
 	
 	for ( i = 0; i <= stringIn.length; i++ ){
-		if ( stringIn.charAt(i) == "B" ){ count += 1; };	
+		if ( stringIn.charAt(i) == charIn ){ count += 1; };	
 	}
 	
 	return count;
-}
+	
+};
+
+console.log("count B's of BbBbBbB(4): " + countBs("BbBbBbB"));
