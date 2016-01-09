0f3e55f2c1fd2215ca8fa17bd2b8cbeed79d3bc7
whediger <wes.hediger@gmail.com>
2016-01-09 01:13:30 -0700

    create sum and range functions

--- theSumOfARange.js
+++ theSumOfARange.js
@@ -1,6 +1,21 @@
 
-
 function range( start, end ){
-	
-	
+
+	var rangeArray = [];
+		
+	for ( i = start; i <= end; i++ ) {
+		rangeArray.push(i);
+	}
+	return rangeArray;
 }
+
+function sum(numbers) {
+	
+	var total = 0;
+	
+	for ( i = 0; i < numbers.length; i++ ) {
+		total += numbers[i];
+	}
+	return total;
+}
+console.log("sum of range 1-10: " + sum(range(1,10)));
