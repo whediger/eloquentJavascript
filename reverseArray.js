5fb80728165616a0beae87c040d8092604cafda5
whediger <wes.hediger@gmail.com>
2016-01-09 20:09:06 -0700

    reverseArray final

--- reverseArray.js
+++ reverseArray.js
@@ -4,5 +4,13 @@ function reverseArray(arrayIn){
 	
 	var arrayOut = [];
 	
-	
+	for ( i = 0; i <= arrayIn.length; i++ ) {
+		arrayOut[i] = arrayIn[(arrayIn.length - i)];
+		console.log("arrayOut: " + arrayOut[i]);
+		
+	}
+	return arrayOut;
 }
+
+var testArray = ["1", "2", "3", "4", "5"];
+console.log("result of passing arroy, contents chars 1 though 5: " + reverseArray(testArray));
