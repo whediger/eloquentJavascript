c699ad7b16b53a4c1383c18b249684ae3fc3aaa5
whediger <wes.hediger@gmail.com>
2016-01-14 01:20:15 -0700

    remove excess console.logs

--- chapters1Through4/deepComparison.js
+++ chapters1Through4/deepComparison.js
@@ -14,7 +14,6 @@ function deepEqual(value1,value2){
 		var obj1prop = [];
 		var obj2prop = [];
 		for (var key in value1) {
-			console.log(key);
 			obj1prop.push(key);
 		}
 		for (var key in value2) {
@@ -25,26 +24,33 @@ function deepEqual(value1,value2){
 			var valCount = 0;
 			for ( i = 0; i < obj1prop.length; i++ ) {
 				for ( ii = 0; ii < obj2prop.length; ii++ ) {	
-					if ( deepEqual(obj1prop[i],obj2prop[ii]) ) { count += 1; }
-				
+					if ( obj1prop[i] == obj2prop[ii] ) { count += 1; }
 				} //searches through for match in all of second objects properties	
 			} //increments through all of first objects properties
-			console.log("count: " + count);
-			console.log("obj 1 length: " + obj1prop.length);
+		
 			if ( count == obj1prop.length ){
 				for ( i = 0; i < obj1prop.length; i++ ) {
-				for ( ii = 0; ii < obj2prop.length; ii++ ) {	
-					if ( deepEqual(value1[obj1prop[i]],value2[obj2prop[ii]]) ) { valCount += 1; }
-				console.log( value1[obj1prop[i]]);
+					for ( ii = 0; ii < obj2prop.length; ii++ ) {	
+						if ( value1[obj1prop[i]] == value2[obj2prop[ii]] ) { valCount += 1; }
 					} //searches through for match in all of second objects values	
 				} //increments through all of first objects values
-				if ( deepEqual(valCount, obj1prop.length)) { console.log("they match"); return true; }
+				if ( valCount == obj1prop.length) { 
+					console.log("returns: true"); 
+					return true; 
+				} else {
+					console.log("returns: false");
+					return false;
+				}
 			}
-		} else { return false;}
+			
+		} else { console.log("returns: false"); 
+				return false;
+				}
 	} else {
 		if ( value1 === value2 ) { 
-			console.log("the values have equal values: " 
-						+ value1 + ":" + value2); return true;} // <---remove console.log()
+			console.log("returns: true"); 
+			return true;
+		}
 	}
 	
 }
@@ -56,5 +62,22 @@ var stuff02 = { value01: "hello", value02: "whatsup"};
 var stuff05 = { value01: "alright", value02: "good"};
 var stuff06 = { value01: "noway", value02: "good"};
 
+// matching objects with more than one value that match but are not in the same order
+var stuff08 = { value02: "whatsup", value01: "hello"};
+var stuff09 = { value01: "hello", value02: "whatsup"};
+
+// matching values but one object has an extra value
+var stuff11 = { value01: "hello", value02: "whatsup"};
+var stuff12 = { value01: "hello", value02: "whatsup", value03: "more stuff"};
+
+console.log("matching comparison: ");
 deepEqual(stuff01, stuff02);
 
+console.log("unmatching comparison: ");
+deepEqual(stuff05, stuff06);
+
+console.log("matching but scrambled comparison: ");
+deepEqual(stuff08, stuff09);
+
+console.log("matching values but one has an extra value: ");
+deepEqual(stuff11,stuff12);
