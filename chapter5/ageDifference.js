c3fed2ecf6fe22c5143aa5b2eed0010965137fae
whediger <wes.hediger@gmail.com>
2016-01-29 01:23:54 -0700

    ageDifference final

--- chapter5/ageDifference.js
+++ chapter5/ageDifference.js
@@ -1,14 +1,13 @@
 
-
-//this uses the ancestery.js data set file -- JSON file
+//link ancestery.js data set file -- JSON file
 var ancestry = require('./ancestry.js');
 ancestry = JSON.parse(ancestry);
 
-//console.log(ancestry.length);
-
+//searches for and returns values
 function filter(array, test) {
 	
 	var passed = [];
+	
 	for (var i = 0; i < array.length; i++ ){
 		if (test(array[i]))
 			passed.push(array[i]);
@@ -16,10 +15,36 @@ function filter(array, test) {
 	return passed;
 }
 
-filter(ancestry, function(person) {
-	var personBornMother = "Born: " + person.born + " Mother: " + person.mother;
-	console.log(personBornMother);
-	return personBornMother;
-});
+//takes array parameter containing ancestry data of mothers and children
+//returns average age difference
+function averageMotherChildAgeDiff(array) {
+	
+	var ageDifferences = [];
+	var averageAge = 0;
+	var ageTotal = 0;
+	
+	//create array of age differences
+	filter(ancestry, function(person) {
+		
+		var motherName = person.mother;
+		var childBorn = person.born;
+		
+		filter(ancestry, function(person) {
+			if (person.name == motherName ){
+				ageDifferences.push(childBorn - person.born);
+			} 
+		});
+		
+		return ageDifferences;
+	});
+	
+	for (i = 0; i < ageDifferences.length; i++ ) {
+		ageTotal += ageDifferences[i];
+	}
+	
+	averageAge = Math.round( ageTotal / ageDifferences.length );
+	
+	return averageAge;
+}
 
-
+console.log(averageMotherChildAgeDiff(ancestry));
