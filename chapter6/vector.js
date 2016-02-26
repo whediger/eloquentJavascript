7e11c4eb390aae674c25f2651424e37602ebae21
whediger <wes.hediger@gmail.com>
2016-02-26 00:04:15 -0700

    plus function final

--- chapter6/vector.js
+++ chapter6/vector.js
@@ -3,18 +3,32 @@ function Vector(x,y) {
 	this.y = y;
 	
 	this.plus = function(x, y) {
-		vector2 = Math.sqrt(((x*x) + (y*y)));
-		vector2 = Math.round(vector2);
-		console.log("plus executed. vector2: " + vector2);
+		var vector2 = Math.sqrt(((x*x) + (y*y)));
+		//console.log for test purposes
+		console.log("plus executed. vector2: " + vector2.toFixed(2)); //to 2 decimal places for readablility
+		vector2 = vector2 + this.length;
+		//console.log for test purposes
+		console.log("plus executed. vector sum: " + vector2.toFixed(2)); //to 2 decimal places for readablility
+		return vector2;	
 	};
+	
+	
 	
 }
 
+Object.defineProperty(Vector.prototype, "length", {
+	get: function() { return Math.sqrt(((this.x*this.x) + (this.y*this.y)), -2); }
+});
 
+//   +=={=======>   test code follows
+	
 var vec = new Vector(23,3);
 var vec2 = new Vector(11, 5);
 
 console.log(vec.x + " " + vec.y);
 console.log(vec2.x + " " + vec2.y);
-vec.plus(2,3);
+vec.plus(12,3);
+vec2.plus(12,3);
+console.log(vec.length);
+console.log(vec2.length);
 
