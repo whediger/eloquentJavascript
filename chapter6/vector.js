2a185b003c8113241c3881daff15ac74b66d9b81
whediger <wes.hediger@gmail.com>
2016-02-25 22:49:42 -0700

    add plus function

--- chapter6/vector.js
+++ chapter6/vector.js
@@ -1,3 +1,20 @@
 function Vector(x,y) {
+	this.x = x;
+	this.y = y;
+	
+	this.plus = function(x, y) {
+		vector2 = Math.sqrt(((x*x) + (y*y)));
+		vector2 = Math.round(vector2);
+		console.log("plus executed. vector2: " + vector2);
+	};
 	
 }
+
+
+var vec = new Vector(23,3);
+var vec2 = new Vector(11, 5);
+
+console.log(vec.x + " " + vec.y);
+console.log(vec2.x + " " + vec2.y);
+vec.plus(2,3);
+
