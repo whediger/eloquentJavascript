b28f65f2b450a5c5aa58be8daae0da2a0a66d589
whediger <wes.hediger@gmail.com>
2016-02-26 00:11:55 -0700

    add function minus final

--- chapter6/vector.js
+++ chapter6/vector.js
@@ -12,7 +12,15 @@ function Vector(x,y) {
 		return vector2;	
 	};
 	
-	
+	this.minus = function(x, y) {
+		var vector2 = Math.sqrt(((x*x) + (y*y)));
+		//console.log for test purposes
+		console.log("minus executed. vector2: " + vector2.toFixed(2)); //to 2 decimal places for readablility
+		vector2 = this.length - vector2;
+		//console.log for test purposes
+		console.log("minus executed. vector difference: " + vector2.toFixed(2)); //to 2 decimal places for readablility
+		return vector2;	
+	};
 	
 }
 
@@ -31,4 +39,6 @@ vec.plus(12,3);
 vec2.plus(12,3);
 console.log(vec.length);
 console.log(vec2.length);
+vec.minus(12,3);
+vec2.minus(12,3);
 
