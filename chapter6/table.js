1830f5043129ee3eb5f811e46da9a0e11ab1a718
whediger <wes.hediger@gmail.com>
2016-02-26 23:14:57 -0700

    add repeat, TextCell, TC.ptype.minW, TC.ptype.draw

--- chapter6/table.js
+++ chapter6/table.js
@@ -37,3 +37,29 @@ function drawTable(rows) {
 	
 	return rows.map(drawRow).join("\n");
 }
+
+function repeat(string, times) {
+	var result = "";
+	for ( var i = 0; i < times; i++)
+		result += string;
+	return result;	
+}
+
+function TextCell(text) {
+	this.text = test.split("\n");
+}
+
+TextCell.prototype.minWidth = function() {
+	return this.text.reduce(function(width, line){
+		return Math.max(width, line.length);
+	}, 0);
+};
+
+TextCell.prototype.draw = function(width, height) {
+	var result = [];
+	for (var i = 0; i < height; i++) {
+		var line = this.text[i] || "";
+		result.push(line + repeat(" ", width - line.length));
+	}
+	return result;
+};
